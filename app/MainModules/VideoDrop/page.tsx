06010ebfdeb1f91003d4ec77dc0a5898f65d87bd
chandu-sportsfan <chandu.srikakulam@sportsfan360.com>
2026-04-01 23:13:22 +0530

    fixed error

--- app/MainModules/VideoDrop/page.tsx
+++ app/MainModules/VideoDrop/page.tsx
@@ -1,13 +1,26 @@
-import VideoDropComponent from "@/src/components/VideoDropComponent/VideoDropComponent";
 
+
+
+
+import { Suspense } from 'react';
+import VideoDropComponent from "@/src/components/VideoDropComponent/VideoDropComponent";
+const Loading = () => (
+    <div className="container mx-auto px-4 py-8 text-center">
+        <h1 className="text-3xl font-bold mb-6">Loading Form...</h1>
+    </div>
+);
 
 export default function HomePage() {
     return (
-        <div className="min-h-screen bg-black text-white flex flex-col lg:flex-row overflow-x-hidden">
+        <Suspense fallback={<Loading />}>
+  <div className="min-h-screen bg-black text-white flex flex-col lg:flex-row overflow-x-hidden">
        <div className="w-full overflow-x-hidden">
                  <VideoDropComponent />
                </div>
                
         </div>
-    )
+        </Suspense>
+    );
 }
+
+

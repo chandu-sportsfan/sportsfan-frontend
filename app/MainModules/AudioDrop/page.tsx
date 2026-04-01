62db449e5799c1c5fb861b894f95a555657c11f6
chandu-sportsfan <chandu.srikakulam@sportsfan360.com>
2026-04-01 23:10:19 +0530

    fixed error

--- app/MainModules/AudioDrop/page.tsx
+++ app/MainModules/AudioDrop/page.tsx
@@ -1,13 +1,26 @@
+
+
+
+import { Suspense } from 'react';
 import AudioDropComponent from "@/src/components/AudioDropComponent/AudioDropComponent";
 
+const Loading = () => (
+    <div className="container mx-auto px-4 py-8 text-center">
+        <h1 className="text-3xl font-bold mb-6">Loading Form...</h1>
+    </div>
+);
 
 export default function HomePage() {
     return (
-        <div className="min-h-screen bg-black text-white flex flex-col lg:flex-row overflow-x-hidden">
-       <div className="w-full overflow-x-hidden">
-                 <AudioDropComponent />
-               </div>
-               
-        </div>
-    )
+        <Suspense fallback={<Loading />}>
+            <div className="min-h-screen bg-black text-white flex flex-col lg:flex-row overflow-x-hidden">
+                <div className="w-full overflow-x-hidden">
+                    <AudioDropComponent />
+                </div>
+
+            </div>
+        </Suspense>
+    );
 }
+
+

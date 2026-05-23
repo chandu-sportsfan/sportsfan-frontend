75aa3e0a45ab7d86c02aed90840744b84f83f46a
chandu-sportsfan <chandu.srikakulam@sportsfan360.com>
2026-05-23 14:31:04 +0530

    fixed error

--- app/MainModules/HomePage/page.tsx
+++ app/MainModules/HomePage/page.tsx
@@ -56,7 +56,7 @@ export default function HomePage() {
         {/* <Player360CardsSection /> */}
         <SocialFeedSection />
         {/* Added News Center Here */}
-        <NewsCenter />
+        {/* <NewsCenter /> */}
       </div>
     </div>
   );

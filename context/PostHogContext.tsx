916410a9c64dea85c65d888f3fae5a1cde9b4473
Prisha Dureja <prisha.dureja@sportsfan360.com>
2026-06-18 08:40:59 +0530

    fix: revert to hardcoded posthog api key

--- context/PostHogContext.tsx
+++ context/PostHogContext.tsx
@@ -7,7 +7,7 @@ import { useEffect, Suspense } from 'react';
 
 if (typeof window !== 'undefined') {
   console.log("Initializing PostHog with reverse proxy...");
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || '', {
+  posthog.init('phc_AHSjFWHPWvbFSQaGBTTGAni9KjyVQyTVjDrwCSHY5kwa', {
     api_host: '/ingest',
     ui_host: 'https://us.posthog.com',
     person_profiles: 'identified_only',

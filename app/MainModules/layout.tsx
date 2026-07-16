9005808d58e1eeed17fd13bc4ebaa8f652f5d5fa
chandu-sportsfan <chandu.srikakulam@sportsfan360.com>
2026-07-16 11:06:46 +0530

    pulling

--- app/MainModules/layout.tsx
+++ app/MainModules/layout.tsx
@@ -169,7 +169,7 @@ function UserSidebar() {
     },
     // { name: "Fan Zone", icon: "/images/profile.png", href: "/MainModules/Fanszone" },
     {
-      name: "Fan Zone",
+      name: "Athelte",
       icon: (
         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
           <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2" />
@@ -178,7 +178,7 @@ function UserSidebar() {
           <path d="M16 3.13a4 4 0 0 1 0 7.75" />
         </svg>
       ),
-      href: "/MainModules/Fanszone"
+      href: "/MainModules/AtheleteHome"
     }
   ];
 
